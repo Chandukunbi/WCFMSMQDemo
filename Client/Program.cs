422d28a79f2fb0d77f82ab62003d7786678a3228
Chandukunbi <chandukunbi@gmail.com>
2020-12-28 19:41:01 +0530

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TransactionScope ts = new TransactionScope(TransactionScopeOption.Required))
+            {
+                Console.WriteLine("Pushing messages to DemoTx queue");
+                localhost.Service1Client proxy = new localhost.Service1Client();
+                int count;
+                count = 1;
+                proxy.SendMessage(string.Format("This is {0} message from client", count));
+                count = 2;
+                proxy.SendMessage(string.Format("This is {0} message from client", count));
+                count = 3;
+                proxy.SendMessage(string.Format("This is {0} message from client", count));
+                count = 4;
+                proxy.SendMessage(string.Format("This is {0} message from client", count));
+                count = 5;
+                proxy.SendMessage(string.Format("This is {0} message from client", count));
+                ts.Complete();
+                Console.WriteLine("Pushed messages to DemoTx queue");
+            }
+                
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+using System.Messaging;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string queue = ".\\private$\\DemoTx";
+            if(!MessageQueue.Exists(queue))
+                MessageQueue.Create(queue, true);
+
+            ServiceHost sh = new ServiceHost(typeof(DemoLibrary.Service1));
+            sh.Open();
+            Console.WriteLine("Server Started");
+            Console.WriteLine("Press any key to stop the server");
+            Console.ReadKey();
+            Console.WriteLine("Server Stopped");
+        }
+    }
+}
